140e446e10a5ef2dd4aa221c0aa9126c0ccc8549
Kocsis Gábor <ifjkocsis.gabor@gmail.com>
2022-01-08 22:24:30 +0100

    ready

--- src/app.ts
+++ src/app.ts
@@ -10,7 +10,30 @@ import { HumanHero } from './hero';
  * Hozz létre benne 3 HumanHero példányt, szabadon választott adatokkal.
  * @var {HumanHero[]} humans
  */
-export const humans: any[] = [];
+export const humans: HumanHero[] = [
+    {
+        id: 1,
+        name: 'Superman',
+        sex: 'male',
+        age: 28,
+        health: 100
+    },
+    {
+        id: 2,
+        name: 'Batmen',
+        sex: 'male',
+        age: 39,
+        health: 50
+    },
+    {
+        id: 3,
+        name: 'Hulk',
+        sex: 'male',
+        age: 50,
+        health: 1000
+    },
+
+];
 
 /**
  * Állítsd be helyesen a változót!
@@ -19,4 +42,26 @@ export const humans: any[] = [];
  * Hozz létre benne 3 TransformerHero példányt, szabadon választott adatokkal.
  * @var {TransformerHero[]} transformers
  */
-export const transformers: any[] = [];
+export const transformers: TransformerHero[] = [
+    {
+        id: 1,
+        name: 'Bumblebee',
+        wings: 0,
+        wheels: 16,
+        clan: 'Autobots'
+    },
+    {
+        id: 2,
+        name: 'Trailbreaker',
+        wings: 0,
+        wheels: 46,
+        clan: 'Autobots'
+    },
+    {
+        id: 3,
+        name: 'Bluestreak',
+        wings: 0,
+        wheels: 4,
+        clan: 'Autobots'
+    }
+];
